c108dfb0a588e5d766bec3642b26b247d323f2ad
Eduarda Azevedo <dudafazv@gmail.com>
2024-05-15 21:39:54 -0300

    alterei a cor do sistema

--- src/main/java/com/example/application/view/LayoutPrincipal.java
+++ src/main/java/com/example/application/view/LayoutPrincipal.java
@@ -18,39 +18,69 @@ public class LayoutPrincipal extends AppLayout {
         this.securityService = securityService;
         createHeader(); // cria cabeçalho
         createDrawer(); // cria menu vertical
+
     }
 
     private void createHeader() {
         H1 logo = new H1("TRILHAS ELETIVAS");
         logo.addClassNames(
-                LumoUtility.FontSize.LARGE,
-                LumoUtility.Margin.LARGE);
+                LumoUtility.FontSize.XLARGE,
+                LumoUtility.Margin.MEDIUM,
+                "cor");
+
+        // Criar o DrawerToggle e aplicar o estilo personalizado
+
+        DrawerToggle barrinha = new DrawerToggle();
+        barrinha.addClassNames("cor");
+
 
         String u = securityService.getAuthenticatedUser().getUsername();
         Button logout = new Button("Log out " + u, e -> securityService.logout());
+        var header = new HorizontalLayout(barrinha, logo, logout);
 
-        var header = new HorizontalLayout(new DrawerToggle(), logo, logout);
+        logo.addClassNames("cor");
+        logout.addClassName("cor");
 
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         header.expand(logo);
         header.setWidthFull();
         header.addClassNames(
                 LumoUtility.Padding.Vertical.NONE,
-                LumoUtility.Padding.Horizontal.MEDIUM);
+                LumoUtility.Padding.Horizontal.MEDIUM,
+                "add-navbar");
 
         addToNavbar(header);
-
     }
 
     private void createDrawer() {
-        addToDrawer(new VerticalLayout(
-                new RouterLink("Home", HomeView.class),
-                new RouterLink("Cadastrar alunos", CadastroAView.class),
-                new RouterLink("Eletivas", HomeView.class),
-                new RouterLink("Manipular usuários", HomeView.class),
-                new RouterLink("Período de matrícula", HomeView.class),
-                new RouterLink("Relatórios", RelatorioView.class),
-                new RouterLink("Perfil", HomeView.class)
-        ));
+        RouterLink homeLink = new RouterLink("Home", HomeView.class);
+        RouterLink cadastroLink = new RouterLink("Cadastro de Alunos", CadastroAView.class);
+        RouterLink eletivasLink = new RouterLink("Eletivas", EletivasView.class);
+        RouterLink manipularLink = new RouterLink("Manipular Usuários", ManipularAView.class);
+        RouterLink periodoLink = new RouterLink("Período de Matricula", PeriodoMatriculaView.class);
+        RouterLink relatoriosLink = new RouterLink("Relatórios", RelatorioView.class);
+        RouterLink perfilLink = new RouterLink("Perfil", PerfilGestorView.class);
+
+        // Adiciona a classe CSS personalizada aos links
+        homeLink.addClassName("black-link");
+        cadastroLink.addClassName("black-link");
+        eletivasLink.addClassName("black-link");
+        manipularLink.addClassName("black-link");
+        periodoLink.addClassName("black-link");
+        relatoriosLink.addClassName("black-link");
+        perfilLink.addClassName("black-link");
+
+        VerticalLayout drawer = new VerticalLayout(
+                homeLink,
+                cadastroLink,
+                eletivasLink,
+                manipularLink,
+                periodoLink,
+                relatoriosLink,
+                perfilLink
+        );
+
+        // Adiciona o drawer ao layout principal
+        addToDrawer(drawer);
     }
 }
