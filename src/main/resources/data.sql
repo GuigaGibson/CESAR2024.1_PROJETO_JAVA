d5180617654efd6ac22087d06ea5174047b8b6a2
Eduarda Azevedo <dudafazv@gmail.com>
2024-05-21 22:24:37 -0300

    Entrada de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,22 @@
-INSERT INTO ALUNO (ID, VERSION, MATRICULA, NOME, EMAIL, SERIE, TURMA, SENHA) VALUES (1, 1, '20232ADSPL0312', 'João Silva', 'joao.silva@example.com', '1° ANO', 'B', 'senha123');
+INSERT INTO ALUNO (ID, VERSION, MATRICULA, NOME, EMAIL, SERIE, TURMA, SENHA) VALUES
+(1, 1, '20232GTIPL0312', 'João Silva', 'joao.silva@example.com', '1° ANO', 'B', 'senha123'),
+(2, 1, '20232GTIPL0419', 'Maria Eduarda', 'dudaazv@example.com', '2° ANO', 'A', 'jds566'),
+(3, 1, '20232GTIPL0718', 'Rogerio Fernandes', 'rogerio678@example.com', '3° ANO', 'C', 'rgr789'),
+(4, 1, '2023GTIPL0417', 'Mercio Luiz', 'mluiz89@example.com', '2° ANO', 'A', 'mercio209'),
+(5, 1, '20232GTIPL457', 'Julia Ribeiro', 'juribeiro16@example.com', '1° ANO', 'B', 'ju123'),
+(6, 1, '20232GTIPL457', 'Julia Ribeiro', 'juribeiro16@example.com', '1° ANO', 'B', 'ju123'),
+(7, 1, '2024GTIZ1234', 'Gustavo Santos', 'gustavosantos@example.com', '2° ANO', 'A', 'gs456'),
+(8, 1, '20242GTIX4321', 'Amanda Lima', 'amandalima@example.com', '3° ANO', 'B', 'al789'),
+(9, 1, '20244GTI1234', 'Rafaela Souza', 'rafaelasouza@example.com', '1° ANO', 'C', 'rs321'),
+(10, 1, '20246GTIV5432', 'Matheus Oliveira', 'matheusoliveira@example.com', '2° ANO', 'A', 'mo654'),
+(11, 1, '20248GTI1234', 'Larissa Costa', 'larissacosta@example.com', '3° ANO', 'B', 'lc987'),
+(12, 1, '20250GTI9876', 'Fernanda Silva', 'fernandasilva@example.com', '1° ANO', 'D', 'fs123'),
+(13, 1, '20252GTI5678', 'Lucas Rodrigues', 'lucasrodrigues@example.com', '2° ANO', 'C', 'lr456'),
+(14, 1, '20254GTI4321', 'Carolina Oliveira', 'carolinaoliveira@example.com', '3° ANO', 'A', 'co789'),
+(15, 1, '20256GTI9876', 'Pedro Almeida', 'pedroalmeida@example.com', '1° ANO', 'B', 'pa321'),
+(16, 1, '20258GTI5678', 'Mariana Pereira', 'marianapereira@example.com', '2° ANO', 'D', 'mp654'),
+(17, 1, '20260GTI4321', 'Rodrigo Martins', 'rodrigomartins@example.com', '3° ANO', 'C', 'rm987');
+
+
+
+
