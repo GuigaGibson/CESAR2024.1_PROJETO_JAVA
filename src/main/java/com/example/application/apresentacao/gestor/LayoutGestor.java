3f993e62a230c1603a465e401c42b72ad8fe37f5
Eduarda Azevedo <150286997+maduazevedo@users.noreply.github.com>
2024-06-09 04:18:27 -0300

    Update LayoutGestor.java

--- src/main/java/com/example/application/apresentacao/gestor/LayoutGestor.java
+++ src/main/java/com/example/application/apresentacao/gestor/LayoutGestor.java
@@ -61,7 +61,7 @@ public class LayoutGestor extends AppLayout{
         RouterLink cadastroELink = new RouterLink("Cadastro de Eletivas", CadastroEView.class);
         RouterLink manipularLink = new RouterLink("Manipular Usuários", ManipularAView.class);
         RouterLink periodoLink = new RouterLink("Período de Matricula", PeriodoMatriculaView.class);
-        RouterLink relatoriosLink = new RouterLink("Relatórios", RelatorioView.class);
+        RouterLink relatoriosLink = new RouterLink("Relatórios", RelatoriosView.class);
         RouterLink perfilLink = new RouterLink("Perfil do Gestor", PerfilGestorView.class);
 
         // Adiciona a classe CSS personalizada aos links
