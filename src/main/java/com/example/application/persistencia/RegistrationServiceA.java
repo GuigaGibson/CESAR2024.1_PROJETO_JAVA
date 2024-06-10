587257c7f6259e003b413576212fd08d5ae45824
Eduarda Azevedo <150286997+maduazevedo@users.noreply.github.com>
2024-06-10 00:10:48 -0300

    Update RegistrationServiceA.java

--- src/main/java/com/example/application/persistencia/RegistrationServiceA.java
+++ src/main/java/com/example/application/persistencia/RegistrationServiceA.java
@@ -2,14 +2,11 @@ package com.example.application.persistencia;
 
 import com.example.application.entidade.Aluno;
 import com.example.application.entidade.Eletivas;
-import com.example.application.entidade.PeriodoMatricula;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,27 +17,8 @@ public class RegistrationServiceA {
     private AlunoRepository repositoryA;
 
     @Autowired
-    private PeriodoMatriculaRepository  periodoMatriculaRepository;
-
-    @Autowired
     private EletivasRepository eletivasRepository;
 
-
-    public boolean verificarPeriodoMatricula() {
-        LocalDate hoje = LocalDate.now();
-        List<PeriodoMatricula> periodo = periodoMatriculaRepository.findAll();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        if(periodo.size() < 1){
-            throw new RuntimeException("Ainda não temos um período para o início das matrículas");
-        }
-        if (hoje.isBefore(periodo.get(0).getDataInicio())) {
-            throw new RuntimeException("As matriculas só se iniciam a partir de " + periodo.get(0).getDataInicio().format(formatter));
-        } else if (hoje.isAfter(periodo.get(0).getDataFim())) {
-            throw new RuntimeException("As matrículas foram até a data " + periodo.get(0).getDataInicio().format(formatter));
-        }
-        return true;
-    }
 
     public List<String> getSerie() {
         return Arrays.asList("1° ANO", "2° ANO", "3° ANO"); // setar os valores do combox serie
@@ -73,7 +51,6 @@ public class RegistrationServiceA {
             if (eletivaAtual != null && eletivaAtual.equals(novaEletiva)) {
                 throw new IllegalStateException("O aluno já está matriculado nesta eletiva");
             }
-            verificarPeriodoMatricula();
 
             if (eletivaAtual != null) {
                 eletivaAtual.getAlunos().remove(aluno);
@@ -91,6 +68,5 @@ public class RegistrationServiceA {
             throw new IllegalStateException("Não há vagas disponíveis para esta eletiva");
         }
     }
-
 
 }
